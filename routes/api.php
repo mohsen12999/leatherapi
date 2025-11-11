29f51ac2220e2a21544adcd679adb94f554342f0
Mohsen Shabanian <mohsen1299@gmail.com>
2025-11-12 01:12:08 +0330

    add api controller and api route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ButcherController;
+use App\Http\Controllers\Api\LeatherController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('butchers', ButcherController::class);
+Route::apiResource('leathers', LeatherController::class);
